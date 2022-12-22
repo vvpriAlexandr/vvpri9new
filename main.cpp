49182b6d75ce84c931008bcb8ee1c28f0bb6caa6
Alexandr <Alexandr9181@list.ru>
2022-12-22 17:57:30 +0300

    1.3.2

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include "sdt.h"
+#include <cmath>
+#include <iostream>
+#include <locale>
+using namespace std;
+
+int main() {
+	int a;
+	int b;
+
+	printf("Vvedite 2 chisla \n");
+
+	cin >> a;
+	cin >> b;
+	
+	return 0;
+}
